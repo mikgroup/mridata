ffb653b31a53b4419d68f88565d9a45aa889c8b8
frankong <frankong@berkeley.edu>
2018-03-28 20:26:04 -0700

    cleanup storages.

--- src/mridata_org/storages.py
+++ src/mridata_org/storages.py
@@ -1,10 +1,11 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = 'static'
+    location = settings.AWS_STATIC_LOCATION
     
 
 class MediaStorage(S3Boto3Storage):
-    location = 'media'
+    location = settings.AWS_MEDIA_LOCATION
     file_overwrite = False
