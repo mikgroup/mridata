89a0ba163db4ac80746c70f203c2b89f2a1fb3e7
frankong <frankong@berkeley.edu>
2018-08-02 23:04:06 -0700

    Added custom Admin interface to Data, Log and TempData.

--- src/mridata/admin.py
+++ src/mridata/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 from .models import Data, TempData, GeData, PhilipsData, SiemensData, IsmrmrdData, Project, Log
 
-admin.site.register(Data)
-admin.site.register(TempData)
-admin.site.register(GeData)
-admin.site.register(PhilipsData)
-admin.site.register(SiemensData)
-admin.site.register(IsmrmrdData)
+
+class DataAdmin(admin.ModelAdmin):
+    list_filter = (
+        ('project', admin.RelatedOnlyFieldListFilter),
+        ('uploader', admin.RelatedOnlyFieldListFilter),
+    )
+    
+class TempDataAdmin(admin.ModelAdmin):
+    list_filter = (
+        ('project', admin.RelatedOnlyFieldListFilter),
+        ('uploader', admin.RelatedOnlyFieldListFilter),
+    )
+    
+class LogAdmin(admin.ModelAdmin):
+    list_filter = (
+        ('user', admin.RelatedOnlyFieldListFilter),
+    )
+    
+admin.site.register(Data, DataAdmin)
+admin.site.register(GeData, TempDataAdmin)
+admin.site.register(PhilipsData, TempDataAdmin)
+admin.site.register(SiemensData, TempDataAdmin)
+admin.site.register(IsmrmrdData, TempDataAdmin)
 admin.site.register(Project)
-admin.site.register(Log)
+admin.site.register(Log, LogAdmin)
