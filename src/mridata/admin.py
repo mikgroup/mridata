8b9307872333879256fccd0f9bb3ab2d8f4c8887
frankong <frankong@berkeley.edu>
2018-08-03 22:50:27 -0700

    Added Aws data objects to admin interface.

--- src/mridata/admin.py
+++ src/mridata/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Data, TempData, GeData, PhilipsData, SiemensData, IsmrmrdData, Project, Log
+from .models import Data, TempData, GeData, PhilipsData, SiemensData, IsmrmrdData, \
+    Project, Log, GeAwsData, PhilipsAwsData, SiemensAwsData, IsmrmrdAwsData
 
 
 class DataAdmin(admin.ModelAdmin):
@@ -21,8 +22,12 @@ class LogAdmin(admin.ModelAdmin):
     
 admin.site.register(Data, DataAdmin)
 admin.site.register(GeData, TempDataAdmin)
+admin.site.register(GeAwsData, TempDataAdmin)
 admin.site.register(PhilipsData, TempDataAdmin)
+admin.site.register(PhilipsAwsData, TempDataAdmin)
 admin.site.register(SiemensData, TempDataAdmin)
+admin.site.register(SiemensAwsData, TempDataAdmin)
 admin.site.register(IsmrmrdData, TempDataAdmin)
+admin.site.register(IsmrmrdAwsData, TempDataAdmin)
 admin.site.register(Project)
 admin.site.register(Log, LogAdmin)
