3e63821f5d16e869d490497206ee7131b7d92a4e
frankong <frankong@berkeley.edu>
2018-08-05 12:07:05 -0700

    Fixed typo.

--- src/mridata/views.py
+++ src/mridata/views.py
@@ -251,5 +251,5 @@ def get_temp_credentials(request):
     if request.user.is_authenticated:
         client = boto3.client('sts')
         r = client.assume_role(RoleArn='arn:aws:iam::876486404445:role/mridata-assets-s3-uploader',
-                               RoleSessionName=str(reqeust.user))
+                               RoleSessionName=str(request.user))
         return JsonResponse(r['Credentials'])
