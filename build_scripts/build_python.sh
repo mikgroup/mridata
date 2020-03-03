07e54691d89b86e93ca62af57946baa2f1d07f9c
Frank Ong <frankongh@gmail.com>
2020-03-02 17:05:55 -0800

    Add requirement files for python

--- build_scripts/build_python.sh
+++ build_scripts/build_python.sh
@@ -7,29 +7,4 @@ wget --quiet https://repo.continuum.io/miniconda/Miniconda3-4.3.21-Linux-x86_64.
 
 bash Miniconda3-4.3.21-Linux-x86_64.sh -b -p /miniconda
 
-pip install --upgrade pip
-
-pip install \
-    boto3 \
-    celery \
-    Django \
-    django-el-pagination \
-    django-filter \
-    django-registration-redux \
-    django-s3direct \
-    django-storages \
-    h5py \
-    numpy \
-    Pillow \
-    psycopg2 \
-    pycurl \
-    PyXB \
-    redis \
-    django-taggit \
-    django-widget-tweaks \
-    django-crispy-forms \
-    django-bootstrap3
-# ISMRMRD-python
-git clone https://github.com/ismrmrd/ismrmrd-python.git
-cd ismrmrd-python
-python setup.py install
+pip install -r build_scripts/requirements.txt
