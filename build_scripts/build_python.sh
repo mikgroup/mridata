6757954b0ee9436cb3be4d4ef9481d7f9f2a90f4
Frank Ong <frankongh@gmail.com>
2020-03-02 17:20:58 -0800

    Add pip upgrade

--- build_scripts/build_python.sh
+++ build_scripts/build_python.sh
@@ -7,4 +7,6 @@ wget --quiet https://repo.continuum.io/miniconda/Miniconda3-4.3.21-Linux-x86_64.
 
 bash Miniconda3-4.3.21-Linux-x86_64.sh -b -p /miniconda
 
+pip install --upgrade pip
+
 pip install -r build_scripts/requirements.txt
